865de94b21bdef5d7863110e94f89ce37d0907b0
Tomas Alvarez <tomas.alvarez.me@gmail.com>
2013-01-20 15:49:37 -0500

    Reordered db migration sequence

--- db/migrate/20130120035812_add_type_of_to_users.rb
+++ db/migrate/20130120035812_add_type_of_to_users.rb
@@ -1,7 +1,7 @@
 class AddTypeOfToUsers < ActiveRecord::Migration
   def change
-  	remove_column :users, :type
   	add_column :users, :type_of, :string  	
   	remove_column :offers, :type_of
+  	remove_column :users, :type
   end
 end
